58b4c7572f3f90d3c81c1d84ed635fbc3b10d478
0dayZh <0day.zh@gmail.com>
2014-12-29 11:16:38 +0800

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,14 @@
 var router = require('koa-router');
 
 module.exports = function(app) {
+  // mount middleware
+  app.use(router(app));
 
+  // api
+  var APIv1 = new Router();
+
+  // web
+  var web = new Router();
+
+  web.
 }
