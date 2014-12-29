6e79873898106db26f76a30c76e43795e2c40392
0dayZh <0day.zh@gmail.com>
2014-12-29 11:47:25 +0800

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -2,14 +2,21 @@ var router = require('koa-router')
   , mount = require('koa-mount');
 
 module.exports = function(app) {
-  // mount middleware
-  app.use(router(app));
-
   // api
   var APIv1 = new Router();
+
+  APIv1.get('/bitcoin', function *(next) {
+    this.body = "GET bitcoin done.";
+  });
 
   // web
   var web = new Router();
 
-  web.
+  web.get('/', function *(next) {
+    this.body = "GET root path done.";
+  });
+
+  // mount middleware
+  app.use(mount('/v1', APIv1.middleware()));
+     .use(mount('/', web.middleware()));
 }
