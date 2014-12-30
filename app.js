07d7553a61061d04f966c052a85a4ceba3520508
0dayZh <0day.zh@gmail.com>
2014-12-30 22:41:34 +0800

    Add config.js

--- app.js
+++ app.js
@@ -1,9 +1,28 @@
+/**
+ * Module dependencies.
+ */
+
 var koa = require('koa');
 var route = require('./routes/');
 var middlewaires = require('koa-middlewares');
 var app = koa();
 
-// mount routes
+/**
+ * static file server
+ */
+app.use(middlewares.staticCache(path.join(__dirname, 'public'), {
+  buffer: false, //!config.debug,
+  maxAge: 0 //config.debug ? 0 : 60 * 60 * 24 * 7
+}));
+app.use(middlewares.bodyParser());
+
+if (config.debug && process.env.NODE_ENV !== 'test') {
+ app.use(middlewares.logger());
+}
+
+/**
+ * router
+ */
 route(app);
 
 app.listen(3000);
