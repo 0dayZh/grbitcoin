2abb67cdf42375a632a6c76d41bbc4330bc4d9ac
0dayZh <0day.zh@gmail.com>
2014-12-30 22:45:45 +0800

    Update app && Update config.

--- app.js
+++ app.js
@@ -1,29 +1,37 @@
+'use strict';
+
 /**
  * Module dependencies.
  */
 
 var koa = require('koa');
-var route = require('./routes/');
+var routes = require('./routes/');
 var middlewaires = require('koa-middlewares');
+var config = require('config')
 var app = koa();
 
 /**
  * static file server
  */
 app.use(middlewares.staticCache(path.join(__dirname, 'public'), {
-  buffer: false, //!config.debug,
-  maxAge: 0 //config.debug ? 0 : 60 * 60 * 24 * 7
+  buffer: !config.debug,
+  maxAge: config.debug ? 0 : 60 * 60 * 24 * 7
 }));
 app.use(middlewares.bodyParser());
 
 if (config.debug && process.env.NODE_ENV !== 'test') {
- app.use(middlewares.logger());
+  app.use(middlewares.logger());
 }
 
 /**
  * router
  */
-route(app);
+app.use(middlewares.router(app));
+routes(app);
 
-app.listen(3000);
-console.log('grbitcoin listening on port 3000');
+app = module.exports = http.createServer(app.callback());
+
+if (!module.parent) {
+  app.listen(config.port);
+  console.log('$ open http://127.0.0.1:' + config.port);
+}
