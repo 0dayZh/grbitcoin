d21f8827047bc2b2670c8bf245dcee67c8ff960c
0dayZh <0day.zh@gmail.com>
2015-01-04 00:07:11 +0800

    Update mailgun.

--- controllers/web.js
+++ controllers/web.js
@@ -4,6 +4,8 @@
  * Module dependencies.
  */
 var UtilEmail = require('../util-email.js');
+var path = require('path');
+var fs = require('fs');
 
 exports.index = function *(next) {
   yield this.render('index', {});
@@ -13,7 +15,13 @@ exports.sendEmailIfNeeded = function *(next) {
   if (this.checkBody('email').isEmail("Bad email."));
   if (this.errors) {
     console.log(this.errors);
-  } else {
     this.redirect('/');
+  } else {
+    var email = this.request.body.email;
+
+    this.redirect('/');
+
+    var html_body = fs.readFileSync(path.join(__dirname, '../views/email-templates/bind-premailer.html'), 'utf8');
+    UtilEmail.sendEmail(email, 'Confirm your email to get early access', html_body);
   }
 }
--- util-email.js
+++ util-email.js
@@ -1,11 +1,19 @@
-var Mailgun = require('mailgun-js')
+'use strict';
+
+var Mailgun = require('mailgun-js');
 
 var api_key = "key-b6d71506b448a6a59dfd78e4a3917d8b";
-var domain = "sandboxa3c49b6b3174412a91dd7d7d6d4024a4.mailgun.org";
-var from_who = "Mailgun Sandbox <postmaster@sandboxa3c49b6b3174412a91dd7d7d6d4024a4.mailgun.org>";
+var domain = "grbitcoin.com";
+var from_who = "Grbitcoin <postmaster@grbitcoin.com>";
 
-exports.sendEmail = function *(next) {
+exports.sendEmail = function (to_who, subject, html_body) {
   var mailgun = new Mailgun({apiKey: api_key, domain: domain});
+  var data = {
+    from: from_who,
+    to: to_who,
+    subject: subject,
+    html: html_body
+  };
   mailgun.messages().send(data, function(err, body) {
     if (err) {
       console.log("got an error: ", err);
