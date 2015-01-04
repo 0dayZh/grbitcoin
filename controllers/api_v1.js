40fc179d63e55e26871d3b7d830a0e044cee1227
0dayZh <0day.zh@gmail.com>
2015-01-05 00:00:07 +0800

    Upate api.

--- controllers/api_v1.js
+++ controllers/api_v1.js
@@ -1,4 +1,23 @@
+'use strick';
+
+/**
+ * Module dependencies.
+ */
+var mongoose = require('mongoose');
+var Connection = mongoose.model('Connection');
+
 exports.getBitcoinAddress = function *(next) {
-  var email_hash = this.params.email_hash;
-  this.body = "getBitcoinAddress with email hash: " + email_hash;
+  var emailHash = this.params.email_hash;
+  var query = Connection.where({ email_hash: emailHash });
+  query.findOne(function (err, connection) {
+    if (err) {
+      console.log('Error' + err);
+    } else {
+      if (!connection && connection != '') {
+        this.body = 'find resutl.';
+      } else {
+        this.body = 'mo match.';
+      }
+    }
+  });
 }
