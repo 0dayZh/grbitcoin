690f8db7869bf11b5e15960eabae666a8a41d25a
0dayZh <0day.zh@gmail.com>
2015-01-05 12:57:07 +0800

    Update model token.

--- models/token.js
+++ models/token.js
@@ -8,9 +8,11 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var TokenSchema = new Schema({
-  email_hash: { type: String, default: '' },
-  token_string: { type: String, default: '' },
-  expiration_date: { type: Date, default: '' }
+  _id             : { type: mongoose.Schema.ObjectId, select: false },
+  __v             : { type: Number, select: false },
+  email_hash      : { type: String, default: '' },
+  token_string    : { type: String, default: '' },
+  expiration_date : { type: Date, default: '' }
 });
 
 mongoose.model('Token', TokenSchema);
