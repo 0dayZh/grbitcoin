744fe20f9736933a29a6013e2ef7d60c89ebde0a
0dayZh <0day.zh@gmail.com>
2015-01-06 00:55:18 +0800

    Update binded email template.

--- controllers/web.js
+++ controllers/web.js
@@ -23,49 +23,52 @@ exports.sendEmailIfNeeded = function *(next) {
     var email = this.request.body.email;
     email = email.trim().toLowerCase();
 
+    console.log('Checking email: ' + email);
+    // check if the email has been request before.
+    var query = Token.where({ email: email });
+    var token = yield query.findOne().exec();
+    var now = moment();
+
+    if (!token) {
+      // fresh incoming email
+      console.log('Email ( ' + email + ' ) is a fresh incoming one.');
+      var hash = crypto.createHash('md5').update(email).digest('hex');
+      var newToken = new Token({ email: email, token_string: hash, expiration_date: now.add(1, 'day').toDate()});
+      console.log(newToken.toJSON());
+      newToken = yield newToken.save();
+      token = newToken;
+    } else if (moment(token.expiration_date).diff(now) <= 0) {
+      // token expirated
+      console.log('Email ( ' + email + ' ) has been requested before, but the token has expirated.');
+      var hash = crypto.createHash('md5').update(email).digest('hex');
+      token.token_string = hash;
+      token.expiration_date = now.add(1, 'day').toDate();
+      token = yield token.save();
+    } else {
+      // email has been requested before
+      console.log('Email ( ' + email + ' ) has been requested before, and the token is still available.');
+    }
+
     // check if the email has binded to a bitcoin address.
     var query = Connection.where({ email: email });
     var connection = yield query.findOne().exec();
 
-    console.log('Checking email: ' + email);
+    // send email
     if (connection) {
       // binded
       console.log('Email ( ' + email + ' ) has binded.');
-      var html_body = yield this.render('email-templates/binded-premailer', { writeResp: false });
+      var html_body = yield this.render('email-templates/binded-premailer', {
+        writeResp: false,
+        bitcoin_address: connection.bitcoin_address,
+        token_string: token.token_string});
+
       UtilEmail.sendEmail(email, 'Request for your action', html_body);
     } else {
-      // check if the email has been request before.
-      var query = Token.where({ email: email });
-      var token = yield query.findOne().exec();
-      var now = moment();
+      var html_body = yield this.render('email-templates/bind-premailer', {
+        writeResp: false,
+        token_string: token.token_string});
 
-      if (!token) {
-        // fresh incoming email
-        console.log('Email ( ' + email + ' ) is a fresh incoming one.');
-        var hash = crypto.createHash('md5').update(email).digest('hex');
-        var newToken = new Token({ email: email, token_string: hash, expiration_date: now.add(1, 'day').toDate()});
-        console.log(newToken.toJSON());
-        newToken = yield newToken.save();
-
-        var html_body = yield this.render('email-templates/bind-premailer', { writeResp: false });
-        UtilEmail.sendEmail(email, 'Bind to your bitcoin address', html_body);
-      } else if (moment(token.expiration_date).diff(now) <= 0) {
-        // token expirated
-        console.log('Email ( ' + email + ' ) has been requested before, but the token has expirated.');
-        var hash = crypto.createHash('md5').update(email).digest('hex');
-        token.token_string = hash;
-        token.expiration_date = now.add(1, 'day').toDate();
-        token = yield token.save();
-
-        var html_body = yield this.render('email-templates/bind-premailer', { writeResp: false });
-        UtilEmail.sendEmail(email, 'Bind to your bitcoin address', html_body);
-      } else {
-        // email has been requested before
-        console.log('Email ( ' + email + ' ) has been requested before, and the token is still available.');
-        // send to test.
-        var html_body = yield this.render('email-templates/bind-premailer', { writeResp: false });
-        UtilEmail.sendEmail(email, 'Bind to your bitcoin address', html_body);
-      }
+      UtilEmail.sendEmail(email, 'Bind to your bitcoin address', html_body);
     }
   }
 
