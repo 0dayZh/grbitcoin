d96c0cda9033032ea9969804a7f85488f66df65c
0dayZh <0day.zh@gmail.com>
2015-01-05 21:39:38 +0800

    Update.

--- controllers/web.js
+++ controllers/web.js
@@ -7,7 +7,11 @@ var UtilEmail = require('../util-email.js');
 var path = require('path');
 var fs = require('fs');
 var mongoose = require('mongoose');
+var crypto = require('crypto');
+require('date-utils');
+
 var Token = mongoose.model('Token');
+var Connection = mongoose.model('Connection');
 
 exports.index = function *(next) {
   yield this.render('index', {});
@@ -17,14 +21,53 @@ exports.sendEmailIfNeeded = function *(next) {
   if (this.checkBody('email').isEmail("Bad email."));
   if (this.errors) {
     console.log(this.errors);
-    this.redirect('/');
   } else {
     var email = this.request.body.email;
-    // todo @0dayZh, create new Token
+    email = email.trim().toLowerCase();
 
-    this.redirect('/');
+    // check if the email has binded to a bitcoin address.
+    var query = Connection.where({ email: email });
+    var connection = yield query.findOne().exec();
 
-    var html_body = fs.readFileSync(path.join(__dirname, '../views/email-templates/confirm-premailer.html'), 'utf8');
-    UtilEmail.sendEmail(email, 'Confirm your email to get early access', html_body);
+    console.log('Checking email: ' + email);
+    if (connection) {
+      // binded
+      console.log('Email ( ' + email + ' ) has binded.');
+      var html_body = fs.readFileSync(path.join(__dirname, '../views/email-templates/binded-premailer.html'), 'utf8');
+      UtilEmail.sendEmail(email, 'Request for your action', html_body);
+    } else {
+      // check if the email has been request before.
+      var query = Token.where({ email: email });
+      var token = yield query.findOne().exec();
+      var now = Date.now();
+
+      if (!token) {
+        // fresh incoming email
+        console.log('Email ( ' + email + ' ) is a fresh incoming one.');
+        var hash = crypto.createHash('md5').update(email).digest('hex');
+        var newToken = new Token({ email: email, token_string: hash, expiration_date: now.addHours(12)});
+        console.log(newToken.toJSON());
+        newToken = yield newToken.save();
+
+        var html_body = fs.readFileSync(path.join(__dirname, '../views/email-templates/bind-premailer.html'), 'utf8');
+        UtilEmail.sendEmail(email, 'Bind to your bitcoin address', html_body);
+      } else if (Date.compare(token.expiration_date, now) >= 0) {
+        // token expirated
+        console.log('Email ( ' + email + ' ) has been requested before, but the token has expirated.');
+        var hash = crypto.createHash('md5').update(email).digest('hex');
+        token.token_string = hash;
+        token.expiration_date = now.addHours(12);
+        yield token.save();
+
+        var html_body = fs.readFileSync(path.join(__dirname, '../views/email-templates/bind-premailer.html'), 'utf8');
+        UtilEmail.sendEmail(email, 'Bind to your bitcoin address', html_body);
+      } else {
+        // email has been requested before
+        console.log('Email ( ' + email + ' ) has been requested before, and the token is still available.');
+        // ignore.
+      }
+    }
   }
+
+  this.redirect('/');
 }
