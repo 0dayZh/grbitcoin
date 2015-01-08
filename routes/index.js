38c69ee8aeaeb9f74db906379516ea0bab8d0e0c
0dayZh <0day.zh@gmail.com>
2015-01-09 02:21:12 +0800

    Update api routes.

--- routes/index.js
+++ routes/index.js
@@ -11,8 +11,8 @@ module.exports = function(app) {
   var api_v1Router = new Router();
 
   api_v1Router
-    .get('/bitcoin/:email', api_v1.getBitcoinAddress)
-    .get('/email/:bitcoin_address', api_v1.getEmail);
+    .get('/bitcoins/:email', api_v1.getBitcoinAddress)
+    .get('/emails/:bitcoin_address', api_v1.getEmail);
 
   // web
   var webRouter = new Router();
